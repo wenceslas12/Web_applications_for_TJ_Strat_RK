5633fb8a83f242b4ae5b7191ed6a9559d0970412
Václav Buřil <vasekburil@seznam.cz>
2022-11-19 11:30:09 +0100

    1.

---
+++ src/main/java/com/example/web_applications_for_tj_strat_rk/WebApplicationsForTjStratRkApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_applications_for_tj_strat_rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationsForTjStratRkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationsForTjStratRkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_applications_for_tj_strat_rk/WebApplicationsForTjStratRkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_applications_for_tj_strat_rk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationsForTjStratRkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
